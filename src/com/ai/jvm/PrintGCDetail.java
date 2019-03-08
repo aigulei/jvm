b352bb1dea44b3866105ac6a166c53570f38d73b
aigulei <touchjava@126.com>
2019-03-08 16:32:26 +0800

    增加注释

--- src/com/ai/jvm/PrintGCDetail.java
+++ src/com/ai/jvm/PrintGCDetail.java
@@ -1,13 +1,27 @@
 package com.ai.jvm;
 
 /**
+ * 	对象优先分配到Eden
+ * 	大对象直接分配到老年代
+ * 	长期存活的对象分配到老年代
+ * 	空间分配担保
+ * 	动态对象年龄判断
  * 	打印GC日志参数
  * -verbose:gc -verbose:class -XX:+PrintGCDetails
  * -XX:+UseSerialGC
+ * -Xms20M 初始内存大小
+ * -Xmx20M 内存上限
+ * -XX:SurvivoRatio=8
+ * -Xmn20M
  * @author Administrator
  */
 public class PrintGCDetail {
 	public static void main(String[] args) {
 		System.out.println("Hello,World");
+		byte[] b1 = new byte[8*1024*1024];
+		byte[] b2 = new byte[8*1024*1024];
+		byte[] b3 = new byte[8*1024*1024];
+		byte[] b4 = new byte[8*1024*1024];
+		byte[] b5 = new byte[8*1024*1024];
 	}
 }
