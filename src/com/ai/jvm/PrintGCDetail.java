2a47fafbbaaf7b1bca5d529ebbc443f525baf9d9
aigulei <touchjava@126.com>
2019-03-08 11:59:13 +0800

    GC日志

--- src/com/ai/jvm/PrintGCDetail.java
+++ src/com/ai/jvm/PrintGCDetail.java
@@ -4,7 +4,6 @@ package com.ai.jvm;
  * 	打印GC日志参数
  * -verbose:gc -verbose:class -XX:+PrintGCDetails
  * @author Administrator
- *
  */
 public class PrintGCDetail {
 	public static void main(String[] args) {
