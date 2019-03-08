5b23ee5b4b6ab9161ead4151490f7e0be552d544
aigulei <touchjava@126.com>
2019-03-08 11:55:20 +0800

    打印GC日志，class路径

---
+++ src/com/ai/jvm/PrintGCDetail.java
@@ -0,0 +1,12 @@
+package com.ai.jvm;
+
+/**
+ * 打印GC日志，class位置
+ * @author Administrator
+ *
+ */
+public class PrintGCDetail {
+	public static void main(String[] args) {
+		System.out.println("Hello,World");
+	}
+}
